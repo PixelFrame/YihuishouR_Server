3dbb12d80ffe1f2fed3862725d653011f5f2f59d
Pixel Frame <pm421@live.com>
2017-08-06 22:18:46 +0800

    Finish Login & Functions Stand Alone

--- login.php
+++ login.php
@@ -1,46 +1,20 @@
 <?php
-if(isset($_POST['login'])){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	if($con = mysqli_connect('127.0.0.1:3306', 'client', 'clientlogin', 'yihuishour')) {}
-	else {echo 'FAIL TO CONNECT DATABASE';}
-	mysqli_query($con, 'set names utf8');
-	$sql = "select * from user where username = '$username' and password='$password'";
-	$rs = mysqli_query($con, $sql);
-	if($rs == false) {
-		echo 'DATABASE SELECT ERROR';
-	} else if(mysqli_num_rows($rs) == 1){
-		toXml($rs);
+	require "USER_to_XML.php";
+	require "db_config.php";
+	if(isset($_POST['login'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$sql = "select * from user where username = '$username' and password='$password'";
+		$rs = mysqli_query($con, $sql);
+		if($rs == false) {
+			echo 'DATABASE SELECT ERROR';
+		} else if(mysqli_num_rows($rs) == 1){
+			toXml($rs);
+		} else {
+			echo '登录失败';
+		}
 	} else {
-		echo '登录失败';
+		echo '连接成功';
 	}
-} else {
-	echo '连接成功';
-}
-
-function toXml($res){
-	$dom = new DOMDocument("1.0", "utf-8");
-	header("Content-Type: text/xml");
-	$dom->formatOutput = true;
-	
-	$array_field = array();
-	$index = 0;
-	while ($finfo = mysqli_fetch_field($res)){
-		$array_field[$index] = $finfo->name;
-		++$index;
-	}
-	$row = mysqli_fetch_array($res, MYSQLI_ASSOC);
-	$root = $dom->createElement("user");
-	$dom->appendChild($root);
-	$i = 0;
-	foreach($array_field as $fd) {
-		$node[$i] = $dom->createElement($fd);
-		$node[$i]->appendChild($dom->createTextNode($row[$fd]));
-		$root->appendChild($node[$i]);
-		++$i;
-	}
-	echo $dom->saveXML();
-	$dom->save("D:/Server/Log/user.xml");
-}
-
+	exit();
 ?>
--- register.php
+++ register.php
@@ -1,34 +1,34 @@
-<?php  
+<?php
+	require("db_config.php");
+	require("USER_to_XML.php");
 	if(isset($_POST["register"])) {
 		$user = $_POST["username"];  
 		$pwd = $_POST["password"];  
 		$pwd_confirm = $_POST["con_password"];  
 		if($user == "" || $pwd == "" || $pwd_confirm == "") {
-			echo '注册信息不能为空';  
+			echo '注册信息不能为空.-1';  
 		} else {
 			if($pwd == $pwd_confirm) {
-				if($con = mysqli_connect('localhost', 'client', 'clientlogin', 'YihuishouR')) {}
-				else { echo 'FAIL TO CONNECT DATABASE'; }
-				mysqli_query($con, 'set names utf8');  
 				$sql = "select username from user where username = '$user'";
 				$result = mysqli_query($con, $sql);
 				$num = mysqli_num_rows($result); 
 				if($num) {  
-					echo '用户名已存在';  
+					echo '用户名已存在.-1';  
 				} else {
 					$sql_insert = "insert into user (username, password, level) values('$user', '$pwd', '0')";  
 					$res_insert = mysqli_query($con, $sql_insert);  
-					if($res_insert) {  
-						echo '注册成功！';  
-					} else {  
-						echo '系统繁忙，请稍候！';  
+					if($res_insert) {
+						echo '注册成功，请重新登陆.0';
+					} else {
+						echo '系统繁忙，请稍候.-1';  
 					}
 				}
 			} else { 
-				echo '密码不一致！';  
+				echo '密码不一致.-1';  
 			}  
 		}  
 	} else {  
-		echo '提交未成功！';  
+		echo '提交失败.-1';  
 	}
+	exit();
 ?>  
