0607de373b7c5286891c87731aa1c84f8ec318a4
Pixel Frame <pm421@live.com>
2017-08-09 22:17:18 +0800

    Bug Fixes

--- select_avatar.php
+++ select_avatar.php
@@ -2,11 +2,10 @@
     require("db_config.php");
     $id = $_POST["id"];
     $aid = $_POST["aid"];
-    $avatar_path = "http://115.159.188.117/img/Avatar/" + $aid + ".png";
-    $sql = "update user set avatar = '$avatar_path' where id = '$id'";
+    $avatar_path = "http://115.159.188.117/img/Avatar/$aid.png";
+    $sql = "update user set avatar='$avatar_path' where id='$id'";
     $rs = mysqli_query($con, $sql);
     if($rs == false) { echo "数据库错误"; }
-	else if(mysqli_num_rows($rs) != 1) { echo "数据库异常"; }
 	else echo "头像修改成功";
     exit();
 ?>
